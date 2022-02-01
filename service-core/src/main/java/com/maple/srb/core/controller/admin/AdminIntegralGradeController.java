aa690784f6976882b57c657a271af66abec79338
高冠祺 <307590625@qq.com>
2022-02-02 01:30:57 +0800

    swagger2 详细配置

--- service-core/src/main/java/com/maple/srb/core/controller/admin/AdminIntegralGradeController.java
+++ service-core/src/main/java/com/maple/srb/core/controller/admin/AdminIntegralGradeController.java
@@ -4,6 +4,10 @@ package com.maple.srb.core.controller.admin;
 import com.maple.srb.core.mapper.IntegralGradeMapper;
 import com.maple.srb.core.pojo.entity.IntegralGrade;
 import com.maple.srb.core.service.IntegralGradeService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.Tag;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
@@ -17,6 +21,7 @@ import java.util.List;
  * @author ggq
  * @since 2022-02-01
  */
+@Api(tags = "积分等级管理")
 @CrossOrigin  // 跨域
 @RestController
 @RequestMapping("/admin/core/integralGrade")
@@ -25,7 +30,7 @@ public class AdminIntegralGradeController {
     @Resource
     private IntegralGradeService integralGradeService;
 
-
+    @ApiOperation(value = "获取hello world")
     @GetMapping("/hello")
     public String hello(){
         return "hello world";
@@ -36,8 +41,11 @@ public class AdminIntegralGradeController {
         return integralGradeService.list();
     }
 
+    @ApiOperation(value = "根据id删除数据记录", notes = "逻辑删除")
     @DeleteMapping("/remove/{id}")
-    public boolean removeById(@PathVariable Long id){
+    public boolean removeById(
+            @ApiParam(value = "要删除的id", example = "1", required = true)
+            @PathVariable Long id){
         return integralGradeService.removeById(id);
     }
 }
