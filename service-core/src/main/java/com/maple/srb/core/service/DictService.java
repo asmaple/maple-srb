0acf42065e454062049c37affdd1ee6f3777e340
高冠祺 <307590625@qq.com>
2022-02-05 01:27:17 +0800

    集成redis

--- service-core/src/main/java/com/maple/srb/core/service/DictService.java
+++ service-core/src/main/java/com/maple/srb/core/service/DictService.java
@@ -1,9 +1,11 @@
 package com.maple.srb.core.service;
 
+import com.maple.srb.core.pojo.dto.ExcelDictDTO;
 import com.maple.srb.core.pojo.entity.Dict;
 import com.baomidou.mybatisplus.extension.service.IService;
 
 import java.io.InputStream;
+import java.util.List;
 
 /**
  * <p>
@@ -17,4 +19,7 @@ public interface DictService extends IService<Dict> {
 
     void importData(InputStream inputStream);
 
+    List<ExcelDictDTO> listDictData();
+
+    List<Dict> listByParentId(Long parentId);
 }
