481ef67896b2fea8ae83a969ebe731ada7e285c8
高冠祺 <307590625@qq.com>
2022-03-07 11:00:14 +0800

    业务

--- service-core/src/main/java/com/maple/srb/core/service/TransFlowService.java
+++ service-core/src/main/java/com/maple/srb/core/service/TransFlowService.java
@@ -1,7 +1,10 @@
 package com.maple.srb.core.service;
 
+import com.maple.srb.core.pojo.bo.TransFlowBO;
 import com.maple.srb.core.pojo.entity.TransFlow;
 import com.baomidou.mybatisplus.extension.service.IService;
+
+import java.util.List;
 
 /**
  * <p>
@@ -13,4 +16,8 @@ import com.baomidou.mybatisplus.extension.service.IService;
  */
 public interface TransFlowService extends IService<TransFlow> {
 
-}
+    void saveTransFlow(TransFlowBO transFlowBO);
+
+    boolean isSaveTransFlow(String agentBillNo);
+
+    List<TransFlow> selectByUserId(Long userId);}
